256a7356960e339771ad961668cb879d4938d3f0
Juanpe Rojas <juanperojas@gmail.com>
2015-03-03 00:32:27 -0600

    primer commit de lo último que se tiene para la tesis de SGSI

---
+++ salir.php
@@ -0,0 +1,9 @@
+<?php
+    require_once "conf/conf.ini.php";
+    //SALIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIR
+    session_destroy();
+
+    unset($_SESSION["login"]);
+  	session_start();
+    header("location: index.php");
+?>
